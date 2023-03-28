49fb2dfd5fb11c51c5dd64386e632adfff8c3ca7
CryptooEros <cryptooeros@gmail.com>
2023-03-28 09:02:35 +0300

    add validation in target model

--- app/models/target.rb
+++ app/models/target.rb
@@ -2,5 +2,5 @@ class Target < ApplicationRecord
     has_many :foods
     has_many :exercises
     belongs_to :user
-
+    validates :name, inclusion: {in: %w("Lose weight","Maintain weight", "Add weight")}
 end
