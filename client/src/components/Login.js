bbd5540a6e616dea07fb1ec247a7381152bbc2fe
CryptooEros <cryptooeros@gmail.com>
2023-03-29 14:55:52 +0300

    Added login page demo

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-function Login() {
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`Username: ${username}, Password: ${password}`);
+  };
+
   return (
-    <div>
-      
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <h1>Login</h1>
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            className="form-control"
+            id="username"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            required
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Login
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
