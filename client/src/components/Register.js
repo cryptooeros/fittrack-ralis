7375327af2f5d13a163bf71e4ccbe4736a42be86
CryptooEros <cryptooeros@gmail.com>
2023-03-29 16:46:41 +0300

    commit

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react';
+// import './Register.css';
 
 function Register() {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Submitted:', { username, email, password });
+    // TODO: Send form data to backend API
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
+    <form onSubmit={handleSubmit} className="register-form">
+      <h1>Register</h1>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+      </label>
+      <label>
+        Email:
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </label>
+      <button type="submit">Register</button>
+    </form>
+  );
 }
 
-export default Register
+export default Register;
