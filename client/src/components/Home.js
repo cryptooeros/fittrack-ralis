4707303c2df37beb2898de15ad8b3cf42521c00f
CryptooEros <cryptooeros@gmail.com>
2023-03-29 12:30:30 +0300

    home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,21 @@
 import React from 'react'
 
+
 function Home() {
+
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <div>
+        
+        <img className="background"
+          alt="gym"
+          src=""
+        />
+      </div>
+    </>
+  );
+  
+  
 }
 
 export default Home
