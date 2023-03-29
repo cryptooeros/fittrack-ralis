23fe50030bcbe7d7ddc01ec5fde4b712277d032a
CryptooEros <cryptooeros@gmail.com>
2023-03-29 07:03:26 +0300

    Exception Handling

--- app/controllers/targets_controller.rb
+++ app/controllers/targets_controller.rb
@@ -1,17 +1,19 @@
 class TargetsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
     def index
-        render json: Target.all, status: :ok
+      render json: Target.all, status: :ok
     end
 
     def show
-        target = Target.find_by(id: params[:id])
-      
-        if target.present?
-          render json: target, serializer: CustomTargetSerializer, status: :ok
-        else
-          render json: { error: "Target not found" }, status: :not_found
-        end
-      end
-      
+      target = Target.find(params[:id])
+      render json: target, serializer: CustomTargetSerializer, status: :ok
+    end
+
+    private
+
+    def render_not_found_response
+      render json: { error: "Target not found" }, status: :not_found
+    end
+        
 end
