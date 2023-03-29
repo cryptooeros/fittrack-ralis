8424b2ba54471d14fa0a53df130b503cdf9f3038
CryptooEros <cryptooeros@gmail.com>
2023-03-29 06:35:09 +0300

    Added a custom serializer for show targets action

--- app/controllers/targets_controller.rb
+++ app/controllers/targets_controller.rb
@@ -4,5 +4,14 @@ class TargetsController < ApplicationController
         render json: Target.all, status: :ok
     end
 
-
+    def show
+        target = Target.find_by(id: params[:id])
+      
+        if target.present?
+          render json: target, serializer: CustomTargetSerializer, status: :ok
+        else
+          render json: { error: "Target not found" }, status: :not_found
+        end
+      end
+      
 end
