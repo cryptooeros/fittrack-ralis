3e49d11eb182aac80fe25a7f2d3af0858d59abad
CryptooEros <cryptooeros@gmail.com>
2023-03-29 06:48:55 +0300

    added validations to user model and seeded

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
     validates :username, presence: true
     validates :email, presence: true, uniqueness: true
     validates :password_digest, presence: true
+    validates :gender, presence: true
 end
