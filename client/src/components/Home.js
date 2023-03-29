35dbfef2dee038ac0b963df4c8913d5aa5986377
CryptooEros <cryptooeros@gmail.com>
2023-03-29 15:25:27 +0300

    edit home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,13 +5,16 @@ function Home() {
 
   return (
     <>
-      <div>
+      <div className='container'>
         
         <img className="background"
           alt="gym"
-          src=""
+          src="https://t4.ftcdn.net/jpg/00/84/54/71/360_F_84547156_ZFXIAmvcgfHNpIoMBwWQm02YZpQ29SUY.jpg"
         />
+        <div className='topright'> NO PAIN, NO GAIN</div>
+        <div className='bottomleft'> FEEL THE POWER WITHIN</div>
       </div>
+      
     </>
   );
   
