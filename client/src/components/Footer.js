327660f7d89bfbfdf5fdf132747984340ed3c896
CryptooEros <cryptooeros@gmail.com>
2023-03-30 22:25:03 +0300

    modified code for footer and its css

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons'
-import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons' 
 import './Footer.css'
 
 function Footer() {
-  return (
-    <footer className='bg-gray-900 text-white'>
-      <div className='container mx-auto px-6 py-12'>
-        <div className='md:flex md:justify-between'>
-          <div className='mb-6 md:mb-0 md:w-1/3'>
-            <h4 className='font-bold mb-3'>Stay Connected with FitTrack</h4>
+  return ( 
+      <footer className='main-footer "mt-auto"'>
+        <div className='row'>
+          {/* first column */}
+          <div className='col-md-4 text-black'>
+          <h4 className='font-bold mb-3'>Stay Connected with FitTrack</h4>
             <ul className='list-unstyled'>
               <li className='mb-2'>
                 <FontAwesomeIcon icon={faPhone} className='mr-2' />
@@ -22,19 +22,22 @@ function Footer() {
               </li>
               <li className='mb-2'>31st Street, Anytown, Kenya</li>
             </ul>
+
           </div>
-          <div className='md:w-1/3'>
-            <h4 className='font-bold mb-3'>About FitTrack</h4>
+          {/* second column */}
+          <div className='col-md-4 text-black'>
+          <h4 className='font-bold mb-3'>About FitTrack</h4>
             <ul className='list-unstyled'>
               <li className='mb-2'>Track your fitness goals and progress</li>
               <li className='mb-2'>Customizable workout plans</li>
               <li className='mb-2'>Connect with other fitness enthusiasts</li>
             </ul>
+
           </div>
-          <div className='md:w-1/3'>
-            <h4 className='font-bold mb-3'>Follow Us</h4>
-            <div className='flex mb-2'>
-              <a href='https://www.facebook.com/fittrackapp/' target='_blank' rel='noreferrer' className='text-gray-400 hover:text-gray-300 mr-4'>
+          {/* third column */}
+          <div className='col-md-4 text-black'>
+          <h4 className='font-bold mb-3'>Follow Us</h4>
+          <a href='https://www.facebook.com/fittrackapp/' target='_blank' rel='noreferrer' className='text-gray-400 hover:text-gray-300 mr-4'>
                 <FontAwesomeIcon icon={faFacebook} className='w-6 h-6' />
               </a>
               <a href='https://twitter.com/fittrackapp' target='_blank' rel='noreferrer' className='text-gray-400 hover:text-gray-300 mr-4'>
@@ -43,29 +46,14 @@ function Footer() {
               <a href='https://www.instagram.com/fittrackapp/' target='_blank' rel='noreferrer' className='text-gray-400 hover:text-gray-300'>
                 <FontAwesomeIcon icon={faInstagram} className='w-6 h-6' />
               </a>
-            </div>
+
           </div>
+
         </div>
-        <hr className='border-gray-800 my-8' />
-        <div className='flex justify-between items-center'>
-          <p className='text-sm'>
-            &copy;{new Date().getFullYear()} FitTrack | All rights reserved | Terms of Service | Privacy Policy
-            </p>
-          <div className='flex'>
-            <a href='#!' className='text-gray-400 hover:text-gray-300 mr-4'>
-              <FontAwesomeIcon icon={faFacebook} className='w-6 h-6' />
-            </a>
-            <a href='#!' className='text-gray-400 hover:text-gray-300 mr-4'>
-              <FontAwesomeIcon icon={faTwitter} className='w-6 h-6' />
-            </a>
-            <a href='#!' className='text-gray-400 hover:text-gray-300'>
-              <FontAwesomeIcon icon={faInstagram} className='w-6 h-6' />
-            </a>
-          </div>
-        </div>
-      </div>
-    </footer>
-  )
+      </footer>
+    
+ 
+    )
 }
 
 export default Footer
