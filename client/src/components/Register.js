757f9eec5803ee2fb7ed3574c2c05cd3b6e5896d
CryptooEros <cryptooeros@gmail.com>
2023-03-30 07:23:52 +0300

    Modified Register.js and Register.css

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-// import './Register.css';
+import './Register.css';
 
 function Register() {
   const [username, setUsername] = useState('');
@@ -13,22 +13,24 @@ function Register() {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="register-form">
-      <h1>Register</h1>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-      </label>
-      <label>
-        Email:
-        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      </label>
-      <label>
-        Password:
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      </label>
-      <button type="submit">Register</button>
-    </form>
+    <div className="register-container">
+      <form onSubmit={handleSubmit} className="register-form">
+        <h1>Register</h1>
+        <label>
+          Username:
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+        </label>
+        <label>
+          Email:
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </label>
+        <label>
+          Password:
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        <button type="submit">Register</button>
+      </form>
+    </div>
   );
 }
 
