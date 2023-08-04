db957563f329519a51e94fe35b66acfb78a8bf0c
CryptooEros <cryptooeros@gmail.com>
2023-08-04 17:42:09 +0300

    update the login form using tailwind css

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+// import './App.css';
 import { Route, Routes, BrowserRouter, Navigate } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import AboutUs from "./components/AboutUs";
