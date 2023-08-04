2a97c8be1d35f3f61ee4ea05597bc8cbc4d090e3
CryptooEros <cryptooeros@gmail.com>
2023-08-04 18:54:40 +0300

    add logo on sign in page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './Login.css';
 import Swal from 'sweetalert2';
+import { FcSportsMode } from 'react-icons/fc';
 
 
 
@@ -141,16 +142,12 @@ export default function Login() {
       */}
       <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
-          <img
-            className="mx-auto h-12 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt="Your Company"
-          />
+          <FcSportsMode size={40} className="mx-auto h-12 w-auto" />
           <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
           <p className="mt-2 text-center text-sm text-gray-600">
             Or{' '}
-            <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-              start your 14-day free trial
+            <a href="#" className="font-medium text-orange hover:text-amber">
+              sign up to FitTrack
             </a>
           </p>
         </div>
