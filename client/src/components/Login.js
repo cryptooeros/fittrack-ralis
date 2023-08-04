236f155b0fbb913dc487f3ca772b391f74458c2b
CryptooEros <cryptooeros@gmail.com>
2023-08-04 18:20:42 +0300

    add onChange and onSubmit event handlers

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -157,13 +157,15 @@ export default function Login() {
 
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
           <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-            <form className="space-y-6" action="#" method="POST">
+            <form className="space-y-6" action="#" method="POST" onSubmit={handleSubmit}>
               <div>
                 <label htmlFor="email" className="block text-sm font-medium text-gray-700">
                   Email address
                 </label>
                 <div className="mt-1">
                   <input
+                    value={email}
+                    onChange={(event) => setEmail(event.target.value)}
                     id="email"
                     name="email"
                     type="email"
@@ -185,6 +187,8 @@ export default function Login() {
                     type="password"
                     autoComplete="current-password"
                     required
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
                     className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                   />
                 </div>
