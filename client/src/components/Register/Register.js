fda8d620b3a20b783f8d791ad2ca2388c82742d3
CryptooEros <cryptooeros@gmail.com>
2023-08-05 05:20:58 +0300

    add gender field in register form

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -162,6 +162,22 @@ export default function Example() {
               </div>
 
               <div>
+                <label htmlFor="gender" className="block text-sm font-medium text-gray-700">
+                  Gender
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="gender"
+                    name="gender"
+                    type="gender"
+                    autoComplete="gender"
+                    required
+                    className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  />
+                </div>
+              </div>
+
+              <div>
                 <label htmlFor="password" className="block text-sm font-medium text-gray-700">
                   Password
                 </label>
