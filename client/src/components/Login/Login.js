8152a0f5260a2adff693741e1e41eeeb4e1e0756
CryptooEros <cryptooeros@gmail.com>
2023-08-05 05:30:13 +0300

    add focus color of signup link in login component

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -146,8 +146,8 @@ export default function Login() {
           <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
           <p className="mt-2 text-center text-sm text-gray-600">
             Or{' '}
-            <a href="#" className="font-medium text-orange hover:text-amber">
-              sign up to FitTrack
+            <a href="#" className="font-medium text-orange hover:text-amber focus:ring-yellow ">
+              Sign Up to FitTrack
             </a>
           </p>
         </div>
