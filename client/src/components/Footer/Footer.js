e47f26de207612cfec8eee0c3f015481d9b57454
CryptooEros <cryptooeros@gmail.com>
2023-08-06 00:43:04 +0300

    change bg-color of footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -142,7 +142,7 @@ const navigation = {
 
 export default function Footer() {
   return (
-    <footer className="bg-yellow">
+    <footer className="bg-orange">
       <div className="relative mx-auto max-w-7xl overflow-hidden py-20 px-6 sm:py-24 lg:px-8">
         <nav className="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12" aria-label="Footer">
           {navigation.main.map((item) => (
