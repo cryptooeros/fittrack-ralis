b73063abc5b856b4d3f819bb3bbe3de0696b02bd
CryptooEros <cryptooeros@gmail.com>
2023-08-05 05:29:12 +0300

    add focus color of login link in register component

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -117,7 +117,7 @@ export default function Example() {
           <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign Up</h2>
           <p className="mt-2 text-center text-sm text-gray-600">
             Or{' '}
-            <a href="#" className="font-medium text-orange hover:text-amber">
+            <a href="#" className="font-medium text-orange hover:text-amber focus:ring-yellow ">
               Login
             </a>
           </p>
