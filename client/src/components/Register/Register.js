25df5527db683aadf42db5e63332cd51e8a07a68
CryptooEros <cryptooeros@gmail.com>
2023-08-05 05:24:56 +0300

    add FcSportsMode react icon to register.js

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,7 +1,8 @@
-// import React, { useState } from 'react';
-// import './Register.css';
-// import Swal from 'sweetalert2';
-// import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import './Register.css';
+import Swal from 'sweetalert2';
+import { useNavigate } from 'react-router-dom';
+import { FcSportsMode } from 'react-icons/fc';
 
 // function Register() {
 //   const [username, setUsername] = useState('');
@@ -112,11 +113,7 @@ export default function Example() {
       */}
       <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
-          <img
-            className="mx-auto h-12 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt="Your Company"
-          />
+          <FcSportsMode size={40} className="mx-auto h-12 w-auto" />
           <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign Up</h2>
           <p className="mt-2 text-center text-sm text-gray-600">
             Or{' '}
