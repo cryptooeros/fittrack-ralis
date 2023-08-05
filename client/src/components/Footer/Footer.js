0674ce436d99f49bc25de2e08b628cc85d645a4b
CryptooEros <cryptooeros@gmail.com>
2023-08-06 00:57:45 +0300

    add padding to footer navigation

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -155,7 +155,7 @@ export default function Footer() {
         </nav>
         <div className="mt-10 flex justify-center space-x-10">
           {navigation.social.map((item) => (
-            <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500">
+            <a key={item.name} href={item.href} className="text-gray-400 p-20 hover:text-gray-500">
               <span className="sr-only">{item.name}</span>
               <item.icon className="h-6 w-6" aria-hidden="true" />
             </a>
