6578373da3c7de7f94b9d5d2a78129cbaf5f4337
CryptooEros <cryptooeros@gmail.com>
2023-08-05 05:27:56 +0300

    change text color of login link in register component

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -117,8 +117,8 @@ export default function Example() {
           <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign Up</h2>
           <p className="mt-2 text-center text-sm text-gray-600">
             Or{' '}
-            <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-              start your 14-day free trial
+            <a href="#" className="font-medium text-orange hover:text-amber">
+              Login
             </a>
           </p>
         </div>
