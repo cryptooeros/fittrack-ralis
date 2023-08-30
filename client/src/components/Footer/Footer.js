3eb8aef682fd46f43575b784b5f32033f02c538c
CryptooEros <cryptooeros@gmail.com>
2023-08-31 01:35:11 +0300

    add font size to footer nav links

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -147,7 +147,7 @@ export default function Footer() {
         <nav className="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12" aria-label="Footer">
           {navigation.main.map((item) => (
             <div key={item.name} className="pb-4 px-12">
-              <a href={item.href} className="text-sm leading-6 text-gray-600 hover:text-gray-900">
+              <a href={item.href} className="text-lg leading-6 text-gray-600 hover:text-gray-900">
                 {item.name}
               </a>
             </div>
@@ -155,7 +155,7 @@ export default function Footer() {
         </nav>
         <div className="mt-10 flex justify-center space-x-20">
           {navigation.social.map((item) => (
-            <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500">
+            <a key={item.name} href={item.href} className="decoration-gray-400 hover:text-gray-500">
               <span className="sr-only">{item.name}</span>
               <item.icon className="h-6 w-6" aria-hidden="true" />
             </a>
