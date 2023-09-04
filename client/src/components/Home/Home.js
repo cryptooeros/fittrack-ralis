adb1b8de61a0d707e26d2f3c5f6cf9695dc2f1ed
CryptooEros <cryptooeros@gmail.com>
2023-09-05 01:03:47 +0300

    Add stat-section to the homepage

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,72 +1,128 @@
-import React from 'react'
-import Trainer from "../../images/trainer.png"
-// import Exer from "../images/gym-02.png"
+// import React from 'react'
+// import Trainer from "../../images/trainer.png"
+// // import Exer from "../images/gym-02.png"
 
-function Home() {
-  return (
-  <>
-    <div className='maincontainer'>   
-      <div className= "container1">
-        <div className='text-container'>
-        <h2>TIME TO GET <b className='pro'>PROLIFE FITNESS</b></h2>
-        <p className='planding'> Fit<b className='track'>Track</b>, is a fitness website designed to help users maintain a healthy lifestyle by suggesting recommended
-           food and exercises to achieve a specified target</p>
-        </div>
-        <div className='image-container'>
-          <img className= "img" src={Trainer}alt='trainerimage'></img>      
-        </div>      
-      </div> 
-      <h3>
-        <u>Enjoy our services</u>
-      </h3>
-      <div className='container-two'>   
-      <div className='card'> 
-      <h5> EXERCISES AND WORKOUT</h5>      
-          <div className='cardimage'>
-          <img className='exercise'
-          alt="gym"
-          src="https://media.4rgos.it/i/Argos/5021-m0014-m007-m050-asym-m008-m022-smallgymideas-steppers-9190306?maxW=1200&qlt=75&fmt.jpeg.interlaced=true"
-        />
-         </div>
-          <div className='cardtext'>
+// function Home() {
+//   return (
+//   <>
+//     <div className='maincontainer'>   
+//       <div className= "container1">
+//         <div className='text-container'>
+//         <h2>TIME TO GET <b className='pro'>PROLIFE FITNESS</b></h2>
+//         <p className='planding'> Fit<b className='track'>Track</b>, is a fitness website designed to help users maintain a healthy lifestyle by suggesting recommended
+//            food and exercises to achieve a specified target</p>
+//         </div>
+//         <div className='image-container'>
+//           <img className= "img" src={Trainer}alt='trainerimage'></img>      
+//         </div>      
+//       </div> 
+//       <h3>
+//         <u>Enjoy our services</u>
+//       </h3>
+//       <div className='container-two'>   
+//       <div className='card'> 
+//       <h5> EXERCISES AND WORKOUT</h5>      
+//           <div className='cardimage'>
+//           <img className='exercise'
+//           alt="gym"
+//           src="https://media.4rgos.it/i/Argos/5021-m0014-m007-m050-asym-m008-m022-smallgymideas-steppers-9190306?maxW=1200&qlt=75&fmt.jpeg.interlaced=true"
+//         />
+//          </div>
+//           <div className='cardtext'>
             
-            <p>Exercise and workouts are physical activities that are perfomed to improve health, fitness, and overall well-being
-            . They can range from simple activities such as walking, to more intense workouts like weightlifting and high-intensity
-            interval training.
-              </p>
+//             <p>Exercise and workouts are physical activities that are perfomed to improve health, fitness, and overall well-being
+//             . They can range from simple activities such as walking, to more intense workouts like weightlifting and high-intensity
+//             interval training.
+//               </p>
+//           </div>
+//         </div>
+//         <div className='card'>
+//         <h5>FOOD AND NUTRITION</h5>        
+//           <div className='cardimage'>
+//                    <img className="exercise"
+//           alt="gym"
+//           src="https://images.squarespace-cdn.com/content/v1/5d59f351fddabe00013cf3e6/9d48584f-c1a5-414e-addf-403aa6a2d21f/PRE-WORKOUT+NUTRITION.png"
+//         />
+//           </div>
+//           <div className='cardtext'>          
+//           <p>Food and nurtition play an important role in exercise and physical activity. Proper nutrition helps you improve
+//       your atletic perfomances, reduce fatigue , and speed up recovery time after worksouts
+//           </p>
+//           </div>
+//         </div>
+//         <div className='card'> 
+//         <h5>TRACK YOUR PLAN</h5>         
+//           <div className='cardimage'>
+//           <img className="exercise"
+//           alt="gym"
+//           src="https://www.health.com/thmb/0ZP6oyiCMMOhoqm7ykYTqIKCKKA=/2000x0/filters:no_upscale():max_bytes(150000):strip_icc()/fitness-journals-gettyimages-914593896-2000-02a5845d9d4a44e6ba56db2cc4c9566e.jpg"
+//         />
+//           </div>
+//           <div className='cardtext'>          
+//           <p>Setting up your daily targets for exercises will help yo stay motivated and on track towars your fitness goal</p>
+//          </div>
+//         </div>
+//       </div>
+//     </div>
+// </>   
+//   )
+// }
+
+// export default Home
+
+
+
+
+
+
+
+const metrics = [
+  { id: 1, stat: '20+', emphasis: 'Expert', rest: 'coaches at your service.' },
+  { id: 2, stat: '300+', emphasis: 'Members', rest: 'have joined the program.' },
+  { id: 3, stat: '98%', emphasis: 'Customer', rest: 'satisfaction and positive feedback.' },
+  { id: 4, stat: '15+', emphasis: 'Fitness', rest: 'programs and continuing.' },
+]
+
+export default function Example() {
+  return (
+    <div className="relative overflow-hidden bg-gradient-to-b from-green to-yellow">
+      <div className="absolute bottom-0 h-80 w-full xl:inset-0 xl:h-full">
+        <div className="h-full w-full xl:grid xl:grid-cols-2">
+          <div className="h-full xl:relative xl:col-start-2">
+            <img
+              className="h-full w-full object-cover opacity-25 xl:absolute xl:inset-0"
+              src="https://images.unsplash.com/photo-1521737852567-6949f3f9f2b5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2830&q=80&sat=-100"
+              alt="People working on laptops"
+            />
+            <div
+              aria-hidden="true"
+              className="absolute inset-x-0 top-0 h-32 bg-gradient-to-b from-gray-900 xl:inset-y-0 xl:left-0 xl:h-full xl:w-32 xl:bg-gradient-to-r"
+            />
           </div>
         </div>
-        <div className='card'>
-        <h5>FOOD AND NUTRITION</h5>        
-          <div className='cardimage'>
-                   <img className="exercise"
-          alt="gym"
-          src="https://images.squarespace-cdn.com/content/v1/5d59f351fddabe00013cf3e6/9d48584f-c1a5-414e-addf-403aa6a2d21f/PRE-WORKOUT+NUTRITION.png"
-        />
-          </div>
-          <div className='cardtext'>          
-          <p>Food and nurtition play an important role in exercise and physical activity. Proper nutrition helps you improve
-      your atletic perfomances, reduce fatigue , and speed up recovery time after worksouts
+      </div>
+      <div className="mx-auto max-w-4xl px-6 lg:max-w-7xl lg:px-8 xl:grid xl:grid-flow-col-dense xl:grid-cols-2 xl:gap-x-8">
+        <div className="relative pt-12 pb-64 sm:pt-24 sm:pb-64 xl:col-start-1 xl:pb-24">
+          <h2 className="text-base font-bold text-gray">TIME TO GET PROFILE FITNESS</h2>
+          <p className="mt-3 text-3xl font-bold tracking-tight text-white">
+            Shape your ideal body.
           </p>
+          <p className="mt-5 text-2xl text-gray">
+            FitTrack is designed to help you maintain a healthy lifestyle by suggesting recommended
+            food and exercises to achieve a specified target. Here, we will help you shape and build your ideal body and live up your life to the fullest.
+          </p>
+          <div className="mt-12 grid grid-cols-1 gap-y-12 gap-x-6 sm:grid-cols-2">
+            {metrics.map((item) => (
+              <p key={item.id}>
+                <span className="block text-2xl font-bold text-white">{item.stat}</span>
+                <span className="mt-1 block text-lg text-gray">
+                  <span className="font-medium text-white">{item.emphasis}</span> {item.rest}
+                </span>
+              </p>
+            ))}
           </div>
-        </div>
-        <div className='card'> 
-        <h5>TRACK YOUR PLAN</h5>         
-          <div className='cardimage'>
-          <img className="exercise"
-          alt="gym"
-          src="https://www.health.com/thmb/0ZP6oyiCMMOhoqm7ykYTqIKCKKA=/2000x0/filters:no_upscale():max_bytes(150000):strip_icc()/fitness-journals-gettyimages-914593896-2000-02a5845d9d4a44e6ba56db2cc4c9566e.jpg"
-        />
-          </div>
-          <div className='cardtext'>          
-          <p>Setting up your daily targets for exercises will help yo stay motivated and on track towars your fitness goal</p>
-         </div>
         </div>
       </div>
     </div>
-</>   
   )
 }
-
-export default Home
-
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,20 +1,32 @@
 /** @type {import('tailwindcss').Config} */
+
+const { colors: defaultColors } = require('tailwindcss/defaultTheme')
+
+const colors = {
+    ...defaultColors,
+    ...{
+      'orange': '#FF9800',
+      'amber' : '#fbbf24',
+      'yellow': '#eab308',
+      'gray': '#111827',
+      'green':'#22c55e',
+    },
+}
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",'./public/index.html',
   ],
   theme: {
-    extend: {
-      colors:{
-        'orange': '#FF9800',
-        'amber' : '#fbbf24',
-        'yellow': '#eab308',
-        'gray': '#111827',
-        'green':'#22c55e',
-      },
+      colors:colors,
     },
-  },
+    extend: {},
   plugins: [],
 }
 
 // 'orange': '#f97316',
+
+// 'orange': '#FF9800',
+//         'amber' : '#fbbf24',
+//         'yellow': '#eab308',
+//         'gray': '#111827',
+//         'green':'#22c55e',
