70a708be600b3a0836c7b2e71888cb88f0adb9c4
CryptooEros <cryptooeros@gmail.com>
2023-09-05 16:07:29 +0300

    change the content in the testimonial section

--- client/src/components/Home/Testimonials/Testimonials.jsx
+++ client/src/components/Home/Testimonials/Testimonials.jsx
@@ -46,13 +46,12 @@ export default function Example() {
                       <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
                     </svg>
                     <p className="mt-6 text-2xl font-medium text-white">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed urna nulla vitae laoreet augue. Amet
-                      feugiat est integer dolor auctor adipiscing nunc urna, sit.
+                      I made the right choice by choosing FitTrack and by choosing the right plan and program, I already achieved my ideal body.
                     </p>
                   </div>
                   <footer className="mt-6">
-                    <p className="text-base font-medium text-white">Judith Black</p>
-                    <p className="text-base font-medium text-indigo-100">CEO at PureInsights</p>
+                    <p className="text-base font-medium text-white">Samantha Black</p>
+                    <p className="text-base font-medium text-gray">CEO at Barbeau</p>
                   </footer>
                 </blockquote>
               </div>
