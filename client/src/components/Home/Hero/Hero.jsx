ddc98de9f542e4a760c32e72c3fde429e25c9939
CryptooEros <cryptooeros@gmail.com>
2023-09-08 03:07:18 +0300

    reduce padding bottom of the hero section in home page

--- client/src/components/Home/Hero/Hero.jsx
+++ client/src/components/Home/Hero/Hero.jsx
@@ -35,7 +35,7 @@ export default function Example() {
       <section className="relative mb-20">
 
         {/* Hero section */}
-        <div className="pt-16 pb-80 sm:pt-24 sm:pb-40 lg:pt-40 lg:pb-48">
+        <div className="pt-16 pb-80 sm:pt-24 sm:pb-40 lg:pt-40 lg:pb-24">
           <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
             <div className="sm:max-w-lg">
               <h1 className="font text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
