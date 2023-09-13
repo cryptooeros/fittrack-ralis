a3dfa971ce85c1d5a8709434357ee28b38e2051b
CryptooEros <cryptooeros@gmail.com>
2023-09-14 00:57:46 +0300

    add a method to destroy all tables associated with the targets table

--- app/models/target.rb
+++ app/models/target.rb
@@ -1,8 +1,8 @@
 class Target < ApplicationRecord
   belongs_to :user
-  has_many :target_foods
+  has_many :target_foods, dependent: :destroy
   has_many :foods, through: :target_foods
-  has_many :target_exercises
+  has_many :target_exercises, dependent: :destroy
   has_many :exercises, through: :target_exercises
 
   validates :current_weight, presence: true
